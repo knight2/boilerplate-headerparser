ee75df1882f32169fa2abc4e3dce710ce82a7c98
Knight2 <8440420+knight2@users.noreply.github.com>
2018-12-14 11:28:31 -0500

    404 middleware

--- server.js
+++ server.js
@@ -24,10 +24,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// handle WhoAmI api
 app.get("/api/whoami", function(req, res){
   res.json({ipaddres: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
